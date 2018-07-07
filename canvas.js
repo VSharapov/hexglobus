c259362259846175284714cc529e8b0d9202d0fb
Vasiliy Sharapov <vasiliys@gmail.com>
2018-07-07 09:39:17 -0400

    Remove canvas experiment code and add a test hex

--- canvas.js
+++ canvas.js
@@ -1,52 +1,33 @@
 var canvas = document.querySelector('canvas')
 
-canvas.width=window.innerWidth;
+canvas.width=window.innerWidth * (49.0 / 100.0);
 canvas.height=window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-c.fillRect(100, 100, 100, 100);
-
-// Line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 300);
-c.lineTo(500, 500);
-c.strokeStyle = "#f65309";
-c.stroke();
-
-// Arc
-c.beginPath();
-c.arc(300, 300, 30, 30, 0, Math.PI * 2, false);
-c.strokeStyle = "#06f309";
-c.stroke();
-
-var x = 300;
-var dx = 20;
-function animate() {
-	requestAnimationFrame(animate);
-	c.clearRect(0, 0, innerWidth, innerHeight);
-
-	c.beginPath();
-	radius = 30
-	c.arc(x, 300, radius, 30, 0, Math.PI * 2, false);
-	c.strokeStyle = "#06f309";
-	c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-	c.stroke();
-	c.fill();
-
-	if (x + radius > innerWidth || x - radius < 0 ){
-		dx = -dx;
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
 	}
-
-	x+=dx;
+	return color;
 }
 
-animate();
-
-window.addEventListener('resize', function()
-		{
-			canvas.width=window.innerWidth;
-			canvas.height=window.innerHeight;
-		});
+function drawHexagon(centerX, centerY) {
+	r = parseFloat(document.querySelector("input[name='hex-distance']").value)/2;
+	R = r / Math.cos(Math.PI / 6)
+	console.log(r);
+	console.log(R);
+	c.fillStyle = getRandomColor();
+	c.beginPath();
+	c.moveTo(centerX + R, centerY);
+	c.lineTo(centerX + R/2, centerY + r);
+	c.lineTo(centerX - R/2, centerY + r);
+	c.lineTo(centerX - R, centerY);
+	c.lineTo(centerX - R/2, centerY - r);
+	c.lineTo(centerX + R/2, centerY - r);
+	c.closePath();
+	c.fill();
+}
+drawHexagon(0, 0)
