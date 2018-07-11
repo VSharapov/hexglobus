86a7050d5bf0ce281a17506f586e76f1418556a0
Vasiliy Sharapov <vasiliys@gmail.com>
2018-07-10 23:02:36 -0400

    Hex color is now deterministic
    Based on a pseudo-random number generator ( https://gist.github.com/blixt/f17b47c62508be59987b )
    Seed is a simple hash ( http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/ ) of the coordinate string

--- canvas.js
+++ canvas.js
@@ -6,6 +6,23 @@ canvas.height=window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+function stripAlpha(colorString) {
+	if(colorString.length > 7) {
+		return colorString.slice(0, 7);
+	} else {
+		return colorString;
+	}
+}
+
+function getIntColor(num) {
+	// num >>>= 0;
+	// var b = num & 0xFF,
+	//     g = (num & 0xFF00) >>> 8,
+	//     r = (num & 0xFF0000) >>> 16,
+	//     a = (num & 0xFF000000) >>> 24;
+	// return "#" + r.toString(16) + g.toString(16) + b.toString(16) + a.toString(16);
+	return "#" + padZero(num.toString(16), 6);
+}
 
 function getRandomColor() {
 	var letters = '0123456789ABCDEF';
@@ -25,7 +42,7 @@ function invertColor(hex, bw) {
         hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
     }
     if (hex.length !== 6) {
-        throw new Error('Invalid HEX color.');
+        throw new Error('Invalid HEX color.' + hex);
     }
     var r = parseInt(hex.slice(0, 2), 16),
         g = parseInt(hex.slice(2, 4), 16),
@@ -50,10 +67,14 @@ function padZero(str, len) {
     return (zeros + str).slice(-len);
 }
 
-function drawHexagon(centerX, centerY, d, hexText="") {
+function drawHexagon(centerX, centerY, d, hexText="", hexColor="random") {
 	r = d/2;
 	R = r / Math.cos(Math.PI / 6);
-	c.fillStyle = getRandomColor();
+	if(hexColor != "random"){
+		c.fillStyle = hexColor;
+	} else {
+		c.fillStyle = getRandomColor();
+	}
 	c.beginPath();
 	c.moveTo(centerX + R, centerY);
 	c.lineTo(centerX + R/2, centerY + r);
@@ -63,7 +84,8 @@ function drawHexagon(centerX, centerY, d, hexText="") {
 	c.lineTo(centerX + R/2, centerY - r);
 	c.closePath();
 	c.fill();
-	c.font = Math.floor(d/4) + "px Arial";
+	fontFactor = 4; // This is about as big as you can go for printing 3 digit coordinates
+	c.font = Math.floor(d/fontFactor) + "px Arial";
 	c.fillStyle = invertColor(c.fillStyle, 1);
 	c.textAlign = "center";
 	c.textBaseline = "middle";
@@ -74,11 +96,26 @@ function hexCoordsToCanvasCoords(row, col, settings) {
 	x = row * settings.columnSpacing;
 	y = col * settings.rowSpacing;
 	if(row % 2){
-		// even rows are normal
+		// even rows are "normal", odd rows are offset downwards
 	}else{
 		y += settings.rowSpacing / 2;
 	}
 	return [x + canvas.width/2, y + canvas.height/2];
+}
+
+function generateDeterministicColor(i, j) {
+	seedString = "" + i + ", " + j;
+	var tempRNG = new Random(seedString.hashCode());
+	// The prng is pretty simple, so I just added iterations until I stopped seeing patterns
+	randomNumber = tempRNG.next();
+	randomNumber = tempRNG.next();
+	randomNumber = tempRNG.next();
+	randomNumber = tempRNG.next();
+	randomNumber = tempRNG.next();
+	moddedNumber = randomNumber % 0x1000000;
+	flooredNumber = Math.floor(moddedNumber);
+	color = getIntColor(flooredNumber);
+	return color;
 }
 
 function drawScene(settings) {
@@ -90,18 +127,15 @@ function drawScene(settings) {
 	finalVisibleColumn = -firstVisibleColumn;
 	firstVisibleRow = -Math.floor(canvas.height / (2*settings.hexMinorDiameter));
 	finalVisibleRow = -firstVisibleRow;
-	console.log(firstVisibleColumn, finalVisibleColumn, firstVisibleRow, finalVisibleRow);
 	
-	console.time('drawing');
 	for(var i = firstVisibleColumn; i <= finalVisibleColumn; i++){
 		for(var j = firstVisibleRow; j <= finalVisibleRow; j++){
 			coords = hexCoordsToCanvasCoords(i, j, settings);
+			hexColor = generateDeterministicColor(i, j);
 			var hexText = i + ", " + j;
-			// hexText += (i==firstVisibleColumn ? "!" : "");
-			drawHexagon(coords[0], coords[1], settings.hexMinorDiameter, hexText);
+			drawHexagon(coords[0], coords[1], settings.hexMinorDiameter, hexText, hexColor);
 		}
 	}
-	console.timeEnd('drawing');
 }
 
 function main() {
