c7b31837d49e505cc296551eb9ab64f4b41d22e7
Vasiliy Sharapov <vasiliys@gmail.com>
2018-07-16 23:19:42 -0400

    Fixes #3 	Turns out it was possible after all

---
+++ githubIssues.js
@@ -0,0 +1,21 @@
+var urlToGetAllOpenIssues = "https://api.github.com/repos/VSharapov/hexglobus/issues?state=open";
+fetch(urlToGetAllOpenIssues)
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(myJson) {
+		console.log(myJson);
+		myJson.forEach(printIssue);
+	});
+
+function printIssue(issue) {
+	var htmlText = "<a href=\"" + 
+		issue.html_url + 
+		"\"><h3 class=\"issue\">" + 
+		issue.title + 
+		"</h3></a><p class=\"issue\">" + 
+		issue.body + 
+		"</p><br />";
+		document.getElementById('issues').innerHTML += htmlText;
+}
+
